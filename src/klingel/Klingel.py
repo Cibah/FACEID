10ecec660729b8a2995ab1a5bc1cc7883e119f16
Maik Hinkelmann <m.hinkelmann@hotmail.de>
2019-11-04 12:34:51 +0100

    add UDP Server for Klingel

--- src/klingel/Klingel.py
+++ src/klingel/Klingel.py
@@ -1,6 +1,6 @@
-from src.middleware.Middleware import *
+#from src.middleware.Middleware import *
 from src.log.Logger import logger
-
+import socket
 
 def login_and_wait_for_event():
     # Login with credentials
@@ -8,3 +8,21 @@ def login_and_wait_for_event():
     # store session id for other functions?
     # while True: listen for notifications of the button and fire event
     # Send image as parameter: url + "/bha-api/image.cgi"
+
+def udpHandler():
+    UDP_IP_ADDRESS = "0.0.0.0"
+    UDP_PORT_NO = 35344
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+    while True:
+        data = serverSock.recvfrom(1024)
+        message= data[0].decode()
+        print ("Message: ", message)
+        
+def main():
+    #do some udp stuff
+    print("Starting udp server...")
+    udpHandler()
+
+if __name__ == '__main__':
+    main()
