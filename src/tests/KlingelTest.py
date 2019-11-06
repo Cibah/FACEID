fad1000faffc5200c598dc39e1362005c120fc6b
Maik Hinkelmann <m.hinkelmann@hotmail.de>
2019-11-06 12:52:57 +0100

    Update KlingelTest.py

--- src/tests/KlingelTest.py
+++ src/tests/KlingelTest.py
@@ -1,12 +1,48 @@
 import unittest
+from src.klinkel.Klingel import klingel
 
 
-class TestSum(unittest.TestCase):
+class TestKlingel(unittest.TestCase):
 
-    #test functions
     def test_import(self):
-        #define assertiosn from unittest.Testcase
+        #import from config file
+        #define assertions from unittest.Testcase
         #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
+    def test_bind_socket(self):
+        #socket test
+        #test functions
+       
+    def test_readData(self):
+        #read data from socket
+        #define assertions from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+   
+    def test_decryptData(self):
+        #decrypt/parse udp data
+        #define assertions from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    
+    def test_webLogin(self):
+        #login to Doorbird Web API
+        #define assertions from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+        
+    def test_httpRequest(self):
+        #Send http Request for Doorbird picture
+        #define assertions from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6"
+        
+    def test_httpResponse(self):
+        #Read httpResponse from Doorbird API, depends on httpRequest
+        #define assertions from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+        
+    def test_saveImage(self):
+        #save Image returned from httpResponse to storage
+        #define assertions from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+ 
+    
 if __name__ == '__main__':
     unittest.main()
