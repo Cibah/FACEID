c1249519dac11c65cb6d38f2ccc1f628db009542
Maik Hinkelmann <m.hinkelmann@hotmail.de>
2019-11-06 12:38:28 +0100

    Create tests

---
+++ src/tests/KlingelTest.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    #test functions
+    def test_import(self):
+        #define assertiosn from unittest.Testcase
+        #self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
