4a23f7b88e4ed2d87ead99a70b39e1b398ccce6c
Artur Dick <artur.dick@mailbox.org>
2019-12-01 01:05:32 +0100

    Modify the recognition of qr-codes: First upscale the image with PIL, after this make a greyscale model with opencv and check for qr-codes.

--- src/machine_learning/barcode_scanner.py
+++ src/machine_learning/barcode_scanner.py
@@ -1,51 +1,45 @@
 # import the necessary packages
 # HOW TO USE THIS:  python3 barcode_Scanner.py --image ../../img/test1.png
-import argparse
+# import os
+
+import PIL
 import cv2
+import numpy
+from PIL import Image
 from pyzbar import pyzbar
-from src.config.Configurator import Configurator
-
-# construct the argument parser and parse the arguments
-
-
-# show the output image
-# cv2.imshow("Image", image)
-cv2.waitKey(0)
+from src.log.Logger import logger
 
 
 def findQR(pathToImage):
     isQR = False
     qrkey = ""
 
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-i", "--image", required=True, help="path to input image")
-    # ap.add_argument(pathToImage)
-    # args = vars(ap.parse_args())
+    # First resize the image with PIL
+    im = Image.open(pathToImage)
+    (width, height) = (im.width * 2, im.height * 2)
+    im_resized = im.resize((width, height), resample=PIL.Image.NEAREST)
+    logger.debug("Checking QR Codes in " + pathToImage)
 
-    # load the input image
-    image = cv2.imread(pathToImage)
+    # Convert the PIL Image to opencv Image
+    image = cv2.cvtColor(numpy.array(im_resized), cv2.COLOR_RGB2BGR)
+
+    # Switch to grayscale mode
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # find the barcodes in the image and decode each of the barcodes
     barcodes = pyzbar.decode(image)
-
-    qrkey=""
+    logger.debug("Found barcodes: " + str(barcodes))
     # loop over the detected barcodes
     for barcode in barcodes:
-        # extract the bounding box location of the barcode and draw the
-        # bounding box surrounding the barcode on the image
-        (x, y, w, h) = barcode.rect
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
         # the barcode data is a bytes object so if we want to draw it on
         # our output image we need to convert it to a string first
         barcodeData = barcode.data.decode("utf-8")
-        barcodeType = barcode.type
         isQR = True
-
-        qrkey= barcodeData
-
+        qrkey = barcodeData
         # print the barcode type and data to the terminal
-        print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
-
     result = (isQR, qrkey)
     return result
+
+# for face in os.listdir("../../img/QrCodes/"):
+#    if face.endswith(".jpg") or face.endswith(".png"):
+#        findQR("../../img/QrCodes/"+ face)
