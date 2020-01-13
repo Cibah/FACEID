37247907d84488ac4e3d1fdee8d3d4f112fa6b13
Artur Dick <artur.dick@mailbox.org>
2020-01-13 13:52:36 +0100

    Put config file to /etc/faceid.conf

--- src/config/Configurator.py
+++ src/config/Configurator.py
@@ -3,12 +3,13 @@ import os
 from pathlib import Path
 
 class Configurator:
-    #cwd = os.getcwd()
-    #parent_dir = Path(cwd).parent.parent
-    config_path = os.path.dirname(os.path.abspath(__file__)) #os.path.abspath(str(parent_dir))
+    # cwd = os.getcwd()
+    # parent_dir = Path(cwd).parent.parent
+    config_path = os.path.dirname(os.path.abspath(__file__))  # os.path.abspath(str(parent_dir))
 
-    file = '/config.ini'
-    config_file = config_path + file
+    config_file = '/etc/faceid.conf'
+
+    # config_file = config_path + file
 
     @staticmethod
     def get_config(section):
